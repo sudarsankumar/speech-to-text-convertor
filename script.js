a84ecdbe0fddf303816c0ccf780bb9c0c4a30bc5
Sudarsan Kumar <sudarsanmitc@gmail.com>
2021-02-27 07:14:14 +0530

    Added Source Files

---
+++ script.js
@@ -0,0 +1,28 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+var recognition = new speechRecognition();
+var textbox = $("#textbox")
+var instructions = $("#instructions")
+var content = ''
+recognition.continuous = true
+recognition.onstart = function() {
+    instructions.text("Voice Recognition is On")
+}
+recognition.onspeechend = function() {
+    instructions.text("No Activity")
+}
+recognition.onerror = function() {
+    instructions.text("Try Again")
+}
+recognition.onresult = function(event) {
+    var current = event.resultIndex
+    var transcript = event.results[current][0].transcript
+    content += transcript
+    textbox.val(content)
+}
+$("#start-btn").click(function(event) {
+    if(content.length)
+    {
+        content += ''
+    }
+    recognition.start()
+})
